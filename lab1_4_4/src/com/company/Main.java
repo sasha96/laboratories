43b3eeffde3510092c8e73f870170ccd991433c2
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-09-07 22:57:42 +0300

    four

---
+++ lab1_4_4/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int i = 435;
+        System.out.println(i++);
+        System.out.println(i--);
+        System.out.println(++i);
+        System.out.println(--i);
+    }
+}
