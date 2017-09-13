f808c0e5619c9ced5b6846a75a3c340b4efcf192
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-09-13 23:54:22 +0300

    lab1_6_6

--- lab1_6_6/src/com/company/Main.java
+++ lab1_6_6/src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         double[] array = {-0.8, -0.7, 5.1, 9.7, 16.0, 20.4, 21.9, 22.6, 17.8, 7.3, 4.7, 1.9};//середньомісячна
-        // температура в м. Києві 2015р
+        // температура в м. Києві 2015 р
         double var = 0.;
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
