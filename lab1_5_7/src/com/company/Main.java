19f3efd58f9bae536df6df7883746dea04e58f3a
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-09-13 14:35:17 +0300

    lab1_5_7

--- lab1_5_7/src/com/company/Main.java
+++ lab1_5_7/src/com/company/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int min = 0;
+        System.out.println("введіть довільне число ,програма покаже всі perfect number від 0 і до вашого числа");
         int max = sc.nextInt();
-
         for (min = 1; min < max; min++) {
             int suma = 0;
             for (int j = 1; j < min; j++) {
