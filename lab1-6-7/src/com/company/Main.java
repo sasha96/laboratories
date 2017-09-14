ee673651dce5e56907f0903b4443d2fa9a5ed3cc
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-09-14 19:23:17 +0300

    lab1-6-7

---
+++ lab1-6-7/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Перед вами матриця [4][5]");
+        int matrix[][] = {
+                {1, 1, 1, 3, 4},
+                {2, 1, 3, 1, 2},
+                {2, 2, 3, 4, 1},
+                {3, 3, 3, 1, 4}
+        };
+        for (int arr[] : matrix) {
+            for (int arr2 : arr) {
+                System.out.print(arr2 + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(" Введіть якеcь число від 1 до 4 і я скажу вам його індекс в рядку ");
+        int var = scanner.nextInt();
+        System.out.println("ви ввели чило " + var);
+        String res = null;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == 0) {
+                    if (matrix[i][j] == var) {
+                        System.out.println("Line 1 : " + "[" + j + "]");
+                    }
+                }
+                if (i == 1) {
+                    if (matrix[i][j] == var) {
+                        System.out.println("Line 2 : " + "[" + j + "]");
+                    }
+                }
+                if (i == 2) {
+                    if (matrix[i][j] == var) {
+                        System.out.println("Line 3 : " + "[" + j + "]");
+                    }
+                }
+                if (i == 3) {
+                    if (matrix[i][j] == var) {
+                        System.out.println("Line 4 : " + "[" + j + "]");
+                    }
+                }
+            }
+        }
+    }
+}
+
--- lab1_6_7/src/Main.java
+++ lab1_6_7/src/Main.java
@@ -1,4 +1,3 @@
-package com.company;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -50,5 +49,4 @@ public class Main {
         }
     }
 }
-
 
