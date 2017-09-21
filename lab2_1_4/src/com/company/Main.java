918f9d42e9b097c917b170d1fa99f570f0db8548
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-09-21 15:46:37 +0300

    lab2_1_4 change

--- lab2_1_4/src/com/company/Main.java
+++ lab2_1_4/src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class Main {
@@ -11,15 +11,27 @@ public class Main {
         int result = scanner.nextInt();
         Computer computer[] = new Computer[result];
         System.out.println("your array has length = " + result);
-        System.out.println("write " + result + " numbers and I multiply your numbers on 10%" );
+        System.out.println("write " + result + " numbers and I multiply your numbers on 10%");
 
         for (int i = 0; i < computer.length; i++) {
             computer[i] = new Computer();
             int var = scanner.nextInt();
             computer[i].setPrice(var);
-            System.out.println("your number " + i+1 + " multiply 10% " + computer[i].getPrice());
+            System.out.println("your number " + (i + 1) + " multiply 10% " + computer[i].getPrice());
 
         }
-
     }
+}
+
+class Computer {
+    public double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price + price * 0.1;
+    }
+
 }
