1c2afa0355ccfccc119922c84b344478b6c1b483
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-09-21 19:53:42 +0300

    change lab2_1_4

---
+++ lab2_1_4/src/com/company/Computer.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Computer {
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price + price * 0.1;
+    }
+}
--- lab2_1_4/src/com/company/Main.java
+++ lab2_1_4/src/com/company/Main.java
@@ -23,15 +23,3 @@ public class Main {
     }
 }
 
-class Computer {
-    public double price;
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price + price * 0.1;
-    }
-
-}
