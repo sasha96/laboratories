44f0dbd4aa32f5dfb84fbbc609e8baa52d7eef8b
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-10-11 12:04:37 +0300

    Date fields

--- AnnaLookPlease/src/Main.java
+++ AnnaLookPlease/src/Main.java
@@ -1,5 +1,10 @@
+
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("sasha", 21);
+        Date birthDate = new Date();
+        Person p = new Person("sasha", 21, birthDate);
+
     }
 }
--- AnnaLookPlease/src/Person.java
+++ AnnaLookPlease/src/Person.java
@@ -1,10 +1,15 @@
+import java.util.Date;
+
 final class Person {
     private final String name;
     private final int age;
+    private final Date dateBirthey;
 
-    Person(String name, int age) {
+    Person(String name, int age, Date dateBirthey) {
         this.name = name;
         this.age = age;
+        this.dateBirthey = new Date(dateBirthey.getTime());
+        ;
     }
 
     public String getName() {
@@ -14,4 +19,9 @@ final class Person {
     public int getAge() {
         return age;
     }
+
+    public Date getDateBirthey() {
+        return new Date(dateBirthey.getTime());
+    }
+
 }
