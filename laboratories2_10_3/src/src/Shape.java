7df9e14d64a556dd8ec42d3a9f280cc8116803a0
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-10-13 00:32:14 +0300

    lab2_10_3

--- laboratories2_10_3/src/src/Shape.java
+++ laboratories2_10_3/src/src/Shape.java
@@ -21,4 +21,27 @@ public abstract class Shape extends ShapeColorComparator implements Drawable, Co
     public String toString() {
         return "This is : " + getClass().getSimpleName() + " , color is : " + getShapeColor();
     }
+
+    public static Shape parseShape(String str,String color,int i, int t, int y){
+        Shape shape = null;
+
+        switch (str) {
+            case "Tringle":shape = new Triangle(color,i,t,y);
+        }
+        return shape;
+    }
+    public static Shape parseShape(String str,String color,int i, int t){
+        Shape shape = null;
+
+        switch (str) {
+            case "Rectangle":shape = new Rectangle(color,i,t);
+        }
+        return shape;
+    } public static Shape parseShape(String str,String color,int i){
+        Shape shape = null;
+        switch (str) {
+            case "Circle":shape = new Circle(color,i);
+        }
+        return shape;
+    }
 }
